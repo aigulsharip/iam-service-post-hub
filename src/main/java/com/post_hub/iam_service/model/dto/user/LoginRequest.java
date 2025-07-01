e9e921729897b907e7b4d9bca0b7fbbbebfcaa2c
aigulsharip <aigul.sharip@nu.edu.kz>
2025-07-01 09:46:20 +0500

    added test for AuthService

--- src/main/java/com/post_hub/iam_service/model/dto/user/LoginRequest.java
+++ src/main/java/com/post_hub/iam_service/model/dto/user/LoginRequest.java
@@ -3,11 +3,13 @@ package com.post_hub.iam_service.model.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class LoginRequest implements Serializable{
 
     @Email
--- src/main/java/com/post_hub/iam_service/model/request/user/RegistrationUserRequest.java
+++ src/main/java/com/post_hub/iam_service/model/request/user/RegistrationUserRequest.java
@@ -2,10 +2,11 @@ package com.post_hub.iam_service.model.request.user;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-
+@AllArgsConstructor
 public class RegistrationUserRequest {
 
     @NotBlank
