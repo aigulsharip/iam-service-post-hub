d8fcdc1149bdc2fd70413a9101b151a15148eddf
aigulsharip <aigul.sharip@nu.edu.kz>
2025-07-31 17:28:32 +0500

    added iam-service into consul

--- src/main/java/com/post_hub/iam_service/config/SecurityConfig.java
+++ src/main/java/com/post_hub/iam_service/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
             new AntPathRequestMatcher("/v3/api-docs/**"),
             new AntPathRequestMatcher("/swagger-ui/**"),
             new AntPathRequestMatcher("/swagger-ui.html"),
-            new AntPathRequestMatcher("/webjars/**")
+            new AntPathRequestMatcher("/webjars/**"),
+            new AntPathRequestMatcher("/actuator/**")
     };
 
     @Bean
