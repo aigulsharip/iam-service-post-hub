844f7972b5ecd3a4ded2900bc9c8c38b7e28ab45
aigulsharip <aigul.sharip@nu.edu.kz>
2025-08-07 12:41:36 +0500

    changed iam-service.sh file

--- docker/iam-service.sh
+++ docker/iam-service.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 PROFILE=${PROFILE:-local-idea}
-JAR_NAME=iam-service-0.0.1-SNAPSHOT.jar
 
 echo "Starting service with profile: $PROFILE"
-exec java -jar /srv/$JAR_NAME --spring.profiles.active=$PROFILE
+exec java -jar /srv/iam_service-0.0.1-SNAPSHOT.jar --spring.profiles.active=$PROFILE
